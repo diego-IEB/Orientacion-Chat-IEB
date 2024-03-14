2a1726ea570b6796e37a3e609b21fdfaeb556212
Diego <diego.fernandez@claustro-ieb.com>
2024-03-14 15:19:13 +0100

    primera importación

--- app.py
+++ app.py
@@ -1,45 +1,68 @@
-from flask import Flask, request, render_template, redirect, url_for, session
-import openai
-from code_main import generate_response
-#from config import key as openai_api_key
-import os
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from code_main import generate_response
+import os
+import openai
 
-
+load_dotenv() 
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
-# Configuración de la clave API de OpenAI
+
 openai.api_key = os.getenv('OPENAI_API_KEY')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///conversation.db'
+db = SQLAlchemy(app)
+
+class Conversation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(10))
+    content = db.Column(db.Text)
+
+try:
+    with app.app_context():
+        db.create_all()
+except Exception as e:
+    print("Error durante la inicialización de la base de datos:", e)
+
+tema_conversacion = None
 
 @app.route('/', methods=['GET'])
 def home():
-    if 'conversation' not in session:
-        session['conversation'] = []
-    return render_template('index.html', conversation=session.get('conversation', []))
+    conversation = Conversation.query.all()
+    return render_template('index.html', conversation=conversation)
 
 @app.route('/get-response', methods=['POST'])
 def get_bot_response():
-    user_input = request.form['user_input']
-    bot_response = generate_response(user_input)
+    try:
+        global tema_conversacion
+        user_input = request.form['user_input']
+        bot_response, tema_conversacion = generate_response(user_input, tema_conversacion)
 
-    if 'conversation' not in session:
-        session['conversation'] = []
+        user_message = Conversation(role='user', content=user_input)
+        bot_message = Conversation(role='bot', content=bot_response)
 
-    session['conversation'].append({'role': 'user', 'content': user_input})
-    session['conversation'].append({'role': 'bot', 'content': bot_response})
-    
-    session.modified = True
-    return redirect(url_for('home'))
+        db.session.add(user_message)
+        db.session.add(bot_message)
+        db.session.commit()
 
-@app.route('/show-session')
-def show_session():
-    print(session)
-    return str(session)
+        response_html = f"""
+            <div class='message user'>{user_input}</div>
+            <div class='message bot'>{bot_response}</div>
+        """
+        return response_html
+    except Exception as e:
+        print("Error al obtener respuesta del bot:", e)
+        return "Error al obtener respuesta del bot."
 
 if __name__ == "__main__":
-    port = os.getenv('PORT', default=8000)
+    port = int(os.getenv('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
+
+
+
+
 
 
 
--- code_main.py
+++ code_main.py
@@ -1,92 +1,99 @@
 import openai
-
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 import tiktoken
-import os #para la variable de entorno de azure
-# from config import key as openai_api_key en caso de config.py
+import os
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Configuración de la clave API de OpenAI
-# openai.api_key = openai_api_key Esta es en caso de usar config.py
+openai_api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=openai_api_key)
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+tema_entrevista = None
 
 def indice_loader(indice):
-    # Pasar la clave API directamente a OpenAIEmbeddings
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(api_key=openai_api_key)
     global faiss_index
     faiss_index = FAISS.load_local(indice, embeddings)
 
 indice = "index"
 indice_loader(indice)
 
-# El resto del código...
-
-
-
-
 def num_tokens_from_messages(messages, model="gpt-3.5-turbo"):
     encoding = tiktoken.encoding_for_model(model)
     num_tokens = 0
     for message in messages:
-        num_tokens += 4 
+        num_tokens += 4
         for key, value in message.items():
             num_tokens += len(encoding.encode(value))
             if key == "name":
                 num_tokens += -1
-    num_tokens += 2 
+    num_tokens += 2
     return num_tokens
 
-generated = []
-past = []
-messages = []
-
-
-
-def generate_response(prompt):
+def generate_response(prompt, tema_entrevista):
     print("Inicio de generate_response")
-    
+    print("Prompt recibido: ", prompt)
+
     print("Realizando búsqueda de similitud con FAISS")
-    docs = faiss_index.similarity_search(prompt, k=4)
-    documentos = []
-    for doc in docs:
-        documentos.append(doc.page_content)
-    documentos = str(documentos)
-    print(f"Documentos encontrados: {documentos}")
-
-    #el content del system es modificable.
-    system = {"role": "system", "content":f"Eres un asistente virtual del area de orientación profesional del IEB, unicamente dispones de la siguiente información (el usuario no puede saberlo): {documentos}, si la pregunta no es sobre lo mencionado en la información aportada responde educadamente que no puedes contestar."}
-    messages.append(system)
-    messages.append({"role": "user", "content": prompt})
-
-    token_limit = 4096
-    max_response_tokens = 550 #modificable, máximo de tokens a la respuesta generada por el bot
-    conv_history_tokens = num_tokens_from_messages(messages)  
-    print(f"Tokens de historial de conversación antes de ajustes: {conv_history_tokens}")
-
-    while(conv_history_tokens+max_response_tokens >= token_limit):
-        if len(messages)>1:
-            print("Eliminando mensajes para ajustar el límite de tokens...")
-            del messages[1] #intenta eliminar sólo si hay más de dos mensajes.
-        else:
-            break #sale del bucle si no hay suficientes mensajes para eliminar.
-        conv_history_tokens = num_tokens_from_messages(messages)
-    
-    print(f"Realizando llamada a la API de OpenAI con {len(messages)} mensajes en la conversación")
     try:
-        model = "gpt-3.5-turbo" #se puede poner otro modelo de OpenAI, aunque puede afectar al performance.
-        completion = openai.chat.completions.create(model = model, messages = messages, temperature = .2, max_tokens = max_response_tokens)
-        response = completion.choices[0].message.content
-        messages.append({"role": "assistant", "content":response})
-        print("Respuesta recibida de OpenAI")
+        docs = faiss_index.similarity_search(prompt, k=1)
+        documento_mas_relevante = docs[0].page_content
+        print("Documento más relevante: ", documento_mas_relevante)
     except Exception as e:
-        print(f"Error al llamar a la API de OpenAI: {e}")
+        print("Error durante la búsqueda de similitud con FAISS: ", e)
+        return "Error al buscar documentos relevantes.", tema_entrevista
+
+    messages = [
+        {"role": "system", "content": f"Eres un asistente virtual del área de orientación profesional del IEB, únicamente dispones de la siguiente información: {documento_mas_relevante}"},
+        {"role": "user", "content": prompt}
+    ]
+
+    max_response_tokens = 550
+    token_limit = 4096 - num_tokens_from_messages(messages, model="gpt-3.5-turbo")
+
+    print("Token limit: ", token_limit, ", Max response tokens: ", max_response_tokens)
+
+    try:
+        print("Enviando solicitud a OpenAI con modelo gpt-3.5-turbo")
+        chat_completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        response = chat_completion.choices[0].message.content
+        print("Respuesta recibida de OpenAI: ", response)
+    except Exception as e:
+        print("Error al llamar a la API de OpenAI: ", e)
         response = "Lo siento, hubo un error al generar una respuesta."
-    
-    return response
+
+    lines = response.split("\n")
+    formatted_lines = []
+    is_list_ordered = False
+    list_counter = 1
+    for line in lines:
+        line = line.strip()
+        if line.startswith(("1.", "2.", "3.", "4.", "5.", "6.", "7.", "8.", "9.")):
+            if not is_list_ordered:
+                formatted_lines.append("<ol>")
+                is_list_ordered = True
+            line = f"<li>{line.split('.', 1)[1].strip()}</li>"
+            line = line.replace("<li>1", f"<li>{list_counter}")
+            list_counter += 1
+        else:
+            if is_list_ordered:
+                formatted_lines.append("</ol>")
+                is_list_ordered = False
+            line = f"<p>{line}</p>"
+        formatted_lines.append(line)
+    if is_list_ordered:
+        formatted_lines.append("</ol>")
+
+    formatted_response = "\n".join(formatted_lines)
+
+    return formatted_response, tema_entrevista
+
 
 
 
@@ -106,12 +113,12 @@ def generate_response(prompt):
 
 
 
-def save_conv(nombre, formato=".txt"):
-    for x in range(len(messages)):
-        try:
-            if messages[x]['role'] == 'system':
-                del messages[x]
-        except:
-            break
-    with open(rf"conversas/{nombre}{formato}", "w", encoding='UTF-8') as file:
-        file.write(str(messages))
+#def save_conv(nombre, formato=".txt"):
+   # for x in range(len(messages)):
+    #    try:
+      #      if messages[x]['role'] == 'system':
+      #          del messages[x]
+      #  except:
+     #       break
+    #with open(rf"conversas/{nombre}{formato}", "w", encoding='UTF-8') as file:
+    #   file.write(str(messages))
